e8f91c104783a5179b7f042c5ab9026df2d0c1f7
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2024-07-03 09:19:00 -0300

    first commit

---
+++ src/main/java/maned/wolf/challenge/btgpactual/orderms/ChallengeBtgpactualApplication.java
@@ -0,0 +1,13 @@
+package maned.wolf.challenge.btgpactual.orderms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBtgpactualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBtgpactualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maned/wolf/challenge/btgpactual/orderms/ChallengeBtgpactualApplicationTests.java
@@ -0,0 +1,13 @@
+package maned.wolf.challenge.btgpactual.orderms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBtgpactualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
